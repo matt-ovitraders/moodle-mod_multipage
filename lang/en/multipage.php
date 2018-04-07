ec903d2497f93ab23151b0c12b0f184ad15437ca
Richard Jones <richard@ib-computing.com>
2018-04-08 11:31:38 +1200

    First commit

---
+++ lang/en/multipage.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for multipage
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_multipage
+ * @copyright  2016 Richard Jones <richardnz@outlook.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @see https://github.com/moodlehq/moodle-mod_newmodule
+ *
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'multipage';
+$string['modulenameplural'] = 'multipages';
+$string['modulename_help'] = 'Use the multipage module for... | The multipage module allows...';
+$string['multipage:addinstance'] = 'Add a new multipage';
+$string['multipage:submit'] = 'Submit multipage';
+$string['multipage:view'] = 'View multipage';
+$string['multipagefieldset'] = 'Custom example fieldset';
+$string['multipagename'] = 'multipage name';
+$string['multipagename_help'] = 'This is the content of the help tooltip associated with the multipagename field. Markdown syntax is supported.';
+$string['multipage'] = 'multipage';
+$string['pluginadministration'] = 'multipage administration';
+$string['pluginname'] = 'multipage';
